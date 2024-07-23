297aebd7ffbae85feb7577e009f63dd57626186c
abolfazl1381 <abolfazlandalib@gmail.com>
2024-07-23 12:01:01 +0330

    df

---
+++ test.py
@@ -0,0 +1,25 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("CUDA is not available. Using CPU.")
+
+
+   
+model = AutoModelForCausalLM.from_pretrained("./psyco_assistant")
+tokenizer = AutoTokenizer.from_pretrained("./psyco_assistant")
+inputs = tokenizer(
+    [
+      "what is mental illness in psycology?"
+    ],
+    return_tensors="pt",
+
+).to("cuda")
+
+outputs = model.generate(**inputs, max_new_tokens=128)
+print(outputs)
+print(tokenizer.batch_decode(outputs))
---
+++ tune.py
@@ -0,0 +1,55 @@
+import torch
+from unsloth import FastLanguageModel
+from trl import SFTTrainer
+from transformers import TrainingArguments
+from datasets import load_dataset
+
+MAX_SEQ = 2048
+
+ds = load_dataset("heliosbrahma/mental_health_chatbot_dataset", split='train')
+
+
+model, tokenizer = FastLanguageModel.from_pretrained(model_name = "unsloth/tinyllama-bnb-4bit",
+                                                     max_seq_length = MAX_SEQ, 
+                                                     dtype=None, 
+                                                     load_in_4bit=True)
+
+
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r=16,
+    target_modules=["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj"],
+    lora_alpha=16,
+    lora_dropout=0,
+    bias="none",
+    use_gradient_checkpointing=True,
+    random_state=3407,
+    use_rslora=False,
+    loftq_config=None,
+)
+
+trainer = SFTTrainer(
+    model = model,
+    train_dataset = ds,
+    dataset_text_field = "text",
+    max_seq_length = MAX_SEQ,
+    tokenizer = tokenizer,
+    args = TrainingArguments(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 10,
+        max_steps = 60,
+        fp16 = not torch.cuda.is_bf16_supported(),
+        bf16 = torch.cuda.is_bf16_supported(),
+        logging_steps = 1,
+        output_dir = "unsloth-test",
+        optim = "adamw_8bit",
+        seed = 3407,
+    ),
+)
+trainer.train()
+
+model.save_pretrained("psyco_assistant")
+
+tokenizer.save_pretrained("psyco_assistant")
